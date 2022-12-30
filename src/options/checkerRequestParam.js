2333128caa1632b0914b9ab28b4d9e005684912b
Timur Sevimli <svmlitimur@gmail.com>
2022-12-30 17:35:50 +0300

    Created app

---
+++ src/options/checkerRequestParam.js
@@ -0,0 +1,12 @@
+'use strict';
+const USERAGENTS = require('../sources/useragents.js');
+const randomAgent = USERAGENTS[Math.floor(Math.random() * USERAGENTS.length)];
+const checkerOptions = {
+  timeout: 15000,
+  headers: {
+    'Content-Type': 'application/json',
+    'User-Agent': randomAgent
+  },
+};
+
+module.exports = checkerOptions;
