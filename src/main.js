fd7104a01bbbc276344d348227c35479e6203e60
Timur Sevimli <svmlitimur@gmail.com>
2022-12-31 03:02:19 +0300

    Min/Sec fn's moved to helpers

--- src/main.js
+++ src/main.js
@@ -6,20 +6,7 @@ const proxySources = require('./sources/proxySources.js');
 const { saveAlivesToLog } = require('./helpers/logSaver.js');
 const postgresConfig = require('./database/postgresConfig.js');
 const consoleDescription = require('./helpers/consoleDescription.js');
-
-const toMinute = (seconds) => {
-  const minutes = Math.trunc(seconds / 60);
-  const remainingSeconds = Math.trunc((seconds / 60 - minutes) * 60);
-  return `${minutes}:${remainingSeconds < 10 ? '0' : ''}${remainingSeconds}`;
-};
-
-const measureElapsedTimeOnSec = () => {
-  const startTime = process.hrtime();
-  return () => {
-    const elapsedTime = process.hrtime(startTime);
-    return (elapsedTime[0] + elapsedTime[1] / 1e9).toFixed();
-  };
-};
+const { toMinute, measureElapsedTimeOnSec } = require('./helpers/times.js');
 
 const bootstrap = async () => {
   consoleDescription();
