4c1d26a459c6fc62dc115c8d511b1cf4c2dce792
Timur Sevimli <svmlitimur@gmail.com>
2023-01-01 04:39:35 +0300

    removed yml and update start.sh

--- start.sh
+++ start.sh
@@ -5,20 +5,6 @@ if ! command -v node >/dev/null 2>&1; then
     exit 1
 fi
 
-if ! command -v docker >/dev/null 2>&1; then
-    echo "Docker is not installed"
-fi
-
-container_name=proxy-db
-
-docker-compose up -d
-
-while ! docker exec $container_name which pg_isready
-do
-  printf "\r$(date) - waiting for proxy-db container to start"
-  sleep 2
-done
-
 if [ ! -d "node_modules" ]; then
   npm install
 fi
