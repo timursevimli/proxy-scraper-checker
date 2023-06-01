b0a1d7bd9772bb4d6c526c72cd9bedfda8e11611
Timur Sevimli <svmlitimur@gmail.com>
2023-06-01 11:35:15 +0300

    Change contracts

--- utilities/getGeoInfo.js
+++ utilities/getGeoInfo.js
@@ -20,7 +20,11 @@ const serializeData = ({
 
 module.exports = (proxy, useProxy = false) => new Promise((resolve, reject) => {
   const [host, port] = proxy.split(':');
-  if (!host && !port) return;
+  if (!host && !port) {
+    const err = 'Host or port is undefined!';
+    reject(new Error(err));
+    return;
+  }
   const url = `http://ip-api.com/json/${host}`;
   const timeout = 10000;
   const options = {
@@ -44,15 +48,21 @@ module.exports = (proxy, useProxy = false) => new Promise((resolve, reject) => {
     .then(
       (res) => {
         if (res.status === 200) {
-          return res.json();
+          res.json()
+            .then(
+              (data) => {
+                if (data) {
+                  const result = serializeData(data, host, port);
+                  resolve(result);
+                } else {
+                  const err = 'Geo data not found!';
+                  reject(new Error(err));
+                }
+              },
+              (reason) => reject(reason)
+            )
+            .catch((err) => reject(err));
         }
-      },
-      (reason) => reject(reason)
-    )
-    .then(
-      (data) => {
-        const result = serializeData(data, host, port);
-        resolve(result);
       },
       (reason) => reject(reason)
     )
