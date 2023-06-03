5a2d6e703e343dd7fd8c665882b1a4b6837c689a
Timur Sevimli <svmlitimur@gmail.com>
2023-06-04 00:47:27 +0300

    Clean project dir

--- utilities/getDuration.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-const { performance } = require('node:perf_hooks');
-module.exports = () => {
-  const start = performance.now();
-  return () => Math.floor(performance.now() - start);
-};
