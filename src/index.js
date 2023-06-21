a0db53925ce2d4066a7287268697f0f9e2a7566d
Timur Sevimli <svmlitimur@gmail.com>
2023-06-21 18:46:35 +0300

    Remove hard code and optimised

--- src/index.js
+++ src/index.js
@@ -4,15 +4,29 @@ const checkers = require('./checkers/');
 const scraper = require('./scraper.js');
 const checker = require('./checker.js');
 
-const sequentialCheck = async (proxies, tasks, options) => {
+const logFile = __dirname + '/../logs/proxy-checker.log';
+const appLogger = logger(logFile)(JSON.stringify)('ProxyChecker');
+const errorLog = appLogger('main')('error');
+
+process.on('uncaughtExceptionMonitor', errorLog);
+process.on('uncaughtException', (err) => {
+  console.error(err.stack || err);
+  errorLog(err);
+  setTimeout(process.exit, 0, 1);
+});
+
+const sequentialCheck = async (proxies, tasks, logger, options) => {
+  console.log({ tasks, options });
   for (const task of tasks) {
-    await checker(proxies, task, options);
+    await checker(proxies, task, logger, options);
   }
 };
 
-const parallelCheck = (proxies, tasks, options) =>
+const parallelCheck = (proxies, tasks, logger, options) =>
   new Promise((resolve) => {
-    const promises = tasks.map((task) => checker(proxies, task, options));
+    const promises = tasks.map(
+      (task) => checker(proxies, task, logger, options)
+    );
     Promise.all(promises).finally(resolve);
   });
 
@@ -23,53 +37,28 @@ const finalize = () => {
   }, 1000);
 };
 
-const execution = {
-  multi: {
-    channels: 35,
-    checker: parallelCheck,
-  },
-  single: {
-    channels: 150,
-    checker: sequentialCheck,
-  }
-};
+const executionOptions = { multi: parallelCheck, single: sequentialCheck };
 
 const boot = async ({
-  executionType = 'single',
+  mode = 'single',
   timeout = 10000,
   source = 'proxy_sources.txt',
   test = false,
+  channels = 100,
 } = {}) => {
-  const logFile = __dirname + '/../logs/proxy-checker.log';
-  const appLogger = logger(logFile)(JSON.stringify)('ProxyChecker');
-
-  process.on('uncaughtExceptionMonitor', (err) => {
-    const errorLog = appLogger('boot')('error');
-    errorLog(err);
-  });
-
-  const proxySources = getSource(source);
+  const proxySources = await getSource(source);
   const sources = test ? proxySources.splice(0, 3) : proxySources;
   const proxies = await scraper(sources, appLogger);
-  const settings = execution[executionType];
-  if (settings) {
-    const { checker, channels } = settings;
-    await checker(
-      proxies,
-      Object.values(checkers),
-      appLogger,
-      { timeout, channels }
-    );
+  const executioner = executionOptions[mode];
+  if (executioner) {
+    const tasks = Object.values(checkers);
+    if (mode === 'multi') channels  = (channels / tasks.length).toFixed(0);
+    await executioner(proxies, tasks, appLogger, { timeout, channels });
     finalize();
   } else {
-    const msg = 'Wrong executionType!';
+    const msg = 'Wrong mode, use \'single\' or \'multi\' mode in config.json!';
     throw new Error(msg);
   }
 };
-
-process.on('uncaughtException', (err) => {
-  console.error(err.stack || err);
-  setTimeout(process.exit, 0, 1);
-});
 
 module.exports = boot;
