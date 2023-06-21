a802380166d41ec8c31e7ab6799a2e101eccad71
Timur Sevimli <svmlitimur@gmail.com>
2023-06-21 18:45:42 +0300

    Convert to async and add check file is exist

--- src/utils/getSource.js
+++ src/utils/getSource.js
@@ -1,15 +1,23 @@
 'use strict';
-const fs = require('node:fs');
+const fs = require('node:fs/promises');
 const path = require('node:path');
+
 const sourcesPath = path.join(__dirname, '..', '../sources/');
-module.exports = (file) => {
+
+module.exports = async (file) => {
   const filePath = sourcesPath + file;
+  let exists;
   try {
-    const data = fs.readFileSync(filePath, 'utf8');
+    exists = await fs.access(filePath).then(() => true, () => false);
+  } catch (err) {
+    throw err;
+  }
+  if (!exists) throw new Error(`File name with ${file} not exist!`);
+  try {
+    const data = await fs.readFile(filePath, 'utf8');
     const sources = data.split('\n');
     return sources;
-  } catch (e) {
-    console.error(e);
-    process.exit(1);
+  } catch (err) {
+    throw err;
   }
 };
