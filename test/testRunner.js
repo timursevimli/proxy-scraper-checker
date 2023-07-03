123f8ad805fc0695e7a2545ac8895b324acec2ff
Timur Sevimli <svmlitimur@gmail.com>
2023-07-03 16:40:15 +0300

    Add try-catch

--- test/testRunner.js
+++ test/testRunner.js
@@ -10,5 +10,9 @@ for (const file of testFiles) {
   if (__filename.includes(file)) continue;
   const filePath = path.join(__dirname, file);
   console.log(`Running tests in ${file}`);
-  require(filePath);
+  try {
+    require(filePath);
+  } catch (e) {
+    throw e;
+  }
 }
