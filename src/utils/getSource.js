30c01eb00bb0ce4dc861d5a7b372b1a7bf8f8cbc
Timur Sevimli <svmlitimur@gmail.com>
2023-08-12 01:15:50 +0300

    Refactor getSource function

--- src/utils/getSource.js
+++ src/utils/getSource.js
@@ -1,18 +1,24 @@
 'use strict';
 
-const fsp = require('node:fs/promises');
+const fs = require('node:fs');
 const path = require('node:path');
 
-const sourcesPath = path.join(__dirname, '..', '../sources/');
+const sourcesPath = path.join(process.cwd(), './sources/');
+
+const isExist = async (file) => {
+  const toBool = [() => true, () => false];
+  const exist = await fs.promises.access(file).then(...toBool);
+  return exist;
+};
 
 module.exports = async (file) => {
   const filePath = path.join(sourcesPath, file);
-  const exists = await fsp.access(filePath).then(
-    () => true,
-    () => false,
-  );
+  const exists = await isExist(filePath);
   if (!exists) throw new Error(`File name with ${file} not exist!`);
-  const data = await fsp.readFile(filePath, 'utf8');
-  const sources = data.split('\n');
-  return sources;
+  let datas = '';
+  const rs = fs.createReadStream(filePath, 'utf8');
+  rs.on('data', (chunk) => void (datas += chunk));
+  return new Promise((resolve) => {
+    rs.on('end', () => void resolve(datas.split('\n')));
+  });
 };
