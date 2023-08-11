cd71c6c65eb58af3ed534d5b1b7a5e077696c343
Timur Sevimli <svmlitimur@gmail.com>
2023-08-11 12:51:46 +0300

    Refactor getSource util

--- src/utils/getSource.js
+++ src/utils/getSource.js
@@ -6,19 +6,10 @@ const path = require('node:path');
 const sourcesPath = path.join(__dirname, '..', '../sources/');
 
 module.exports = async (file) => {
-  const filePath = sourcesPath + file;
-  let exists;
-  try {
-    exists = await fs.access(filePath).then(() => true, () => false);
-  } catch (err) {
-    throw err;
-  }
+  const filePath = path.join(sourcesPath, file);
+  const exists = await fs.access(filePath).then(() => true, () => false);
   if (!exists) throw new Error(`File name with ${file} not exist!`);
-  try {
-    const data = await fs.readFile(filePath, 'utf8');
-    const sources = data.split('\n');
-    return sources;
-  } catch (err) {
-    throw err;
-  }
+  const data = await fs.readFile(filePath, 'utf8');
+  const sources = data.split('\n');
+  return sources;
 };
