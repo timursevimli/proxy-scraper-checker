11aeae34bb94127fbe3a0289b06d52d0c17484c2
Timur Sevimli <svmlitimur@gmail.com>
2023-08-13 16:52:53 +0300

    Refactor style to async-await

--- src/utils/getGeoInfo.js
+++ src/utils/getGeoInfo.js
@@ -5,60 +5,30 @@ const randomUAgent = require('./randomUAgent.js');
 
 const serializeData = (data, host, port) => {
   const { city, country, countryCode, region, regionName, isp } = data;
-  return `
-    ${host}:${port}
+  return `${host}:${port}
     ${countryCode}
     ${country}
     ${city}
     ${region}
     ${regionName}
-    ${isp}
-    `;
+    ${isp}`;
 };
 
-module.exports = (proxy, useProxy = false) =>
-  new Promise((resolve, reject) => {
-    const [host, port] = proxy.split(':');
-    if (!host && !port) {
-      const err = 'Host or port is undefined!';
-      reject(new Error(err));
-      return;
-    }
-    const url = `http://ip-api.com/json/${host}`;
-    const timeout = 10000;
-    const options = {
-      timeout,
-      method: 'GET',
-      headers: {
-        'User-agent': randomUAgent(),
-        'Content-Type': 'application/json',
-      },
-    };
-    if (useProxy) {
-      const agent = new Agent({
-        keepAlive: false,
-        timeout,
-        host,
-        port,
-      });
-      options.agent = agent;
-    }
-    fetch(url, options)
-      .then((res) => {
-        if (res.status === 200) {
-          res
-            .json()
-            .then((data) => {
-              if (data) {
-                const result = serializeData(data, host, port);
-                resolve(result);
-              } else {
-                const err = 'Geo data not found!';
-                reject(new Error(err));
-              }
-            }, reject)
-            .catch(reject);
-        }
-      }, reject)
-      .catch(reject);
-  });
+module.exports = async (proxy, useProxy = false) => {
+  const [host, port] = proxy.split(':');
+  const url = `http://ip-api.com/json/${host}`;
+  const options = {
+    method: 'GET',
+    headers: {
+      Connection: 'close',
+      'User-agent': randomUAgent(),
+      'Content-Type': 'application/json',
+    },
+  };
+  if (useProxy) options.agent = new Agent({ host, port });
+  const res = await fetch(url, options);
+  if (res.status !== 200) throw new Error(res.statusText);
+  const data = await res.json();
+  const result = serializeData(data, host, port);
+  return result;
+};
