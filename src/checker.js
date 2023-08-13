460906f7d03fb028a8b5f2da5ff4f4e60a0adf0c
Timur Sevimli <svmlitimur@gmail.com>
2023-08-13 16:55:38 +0300

    Update code style and remove comments

--- src/checker.js
+++ src/checker.js
@@ -5,17 +5,15 @@ const { Queue, logger } = require('./lib/');
 module.exports = (proxies, checker, options) =>
   new Promise((resolve) => {
     const { channels, timeout } = options;
-    const name = checker.name.toUpperCase();
-    logger.show('system', `${name} started!`);
+    const name = checker.name.replace('check', '').toUpperCase();
     const queue = Queue.channels(channels)
-      .timeout(timeout)
       .process(checker)
-      .success((res) => logger.log(res))
-      // .failure((err) => void logger.error(err.message))
+      .success((res) => void logger.log(res))
       .drain(() => {
-        logger.show('system', `${name} is done!`);
+        logger.show('system', `${name} checker is done!`);
         resolve();
       });
 
-    for (const proxy of proxies) queue.add(proxy);
+    logger.show('system', `${name} checker started!`);
+    for (const proxy of proxies) queue.add({ proxy, timeout });
   });
