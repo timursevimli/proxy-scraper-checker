28f0ca6bf26c53207141a7c0c9e48380ada57414
Timur Sevimli <svmlitimur@gmail.com>
2023-08-14 14:00:16 +0300

    Add option of preference for results to be logged

--- src/checker.js
+++ src/checker.js
@@ -4,15 +4,17 @@ const { Queue, logger } = require('./lib/');
 
 module.exports = (proxies, checker, options) =>
   new Promise((resolve) => {
-    const { channels, timeout } = options;
+    const { channels, timeout, logging } = options;
     const name = checker.name.replace('check', '').toUpperCase();
     const queue = Queue.channels(channels)
       .process(checker)
-      .success((res) => void logger.log(res))
       .drain(() => {
         logger.show('system', `${name} checker is done!`);
         resolve();
       });
+
+    if (logging.success) queue.success((res) => void logger.log(res));
+    if (logging.failures) queue.failure((err) => void logger.error(err));
 
     logger.show('system', `${name} checker started!`);
     for (const proxy of proxies) queue.add({ proxy, timeout });
